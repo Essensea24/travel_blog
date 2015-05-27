7be5eeb54b588eb7bc215d6956bbaee5329ad01e
Sophie Chor <sokpheakc@yaho.com>
2015-05-26 19:41:07 -0700

    added country attribute to the articles migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526031746) do
+ActiveRecord::Schema.define(version: 20150527023032) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "country"
   end
 
   create_table "comments", force: :cascade do |t|
